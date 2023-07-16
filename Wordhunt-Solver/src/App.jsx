88516f7cf1c6753403f5827b70ffcfbd06b7377a
Daniel Zhao <zhaodaniel2005@gmail.com>
2023-07-15 22:47:17 -0400

    Started Solver

--- Wordhunt-Solver/src/App.jsx
+++ Wordhunt-Solver/src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState } from "react";
 import NavBar from "./components/NavBar";
-import Button from "./components/Button";
 
 import InputGrid from "./components/InputGrid";
 
@@ -10,7 +9,6 @@ function App() {
 		<div>
 			<NavBar />
 			<InputGrid />
-			<Button />
 		</div>
 	);
 }
