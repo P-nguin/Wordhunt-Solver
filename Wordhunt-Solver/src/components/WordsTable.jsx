962a81c7193db75d27458a77e870978664aa82b4
Ashley Zhao <zhaoashley2005@gmail.com>
2023-08-10 23:54:19 -0400

    i dont know

--- Wordhunt-Solver/src/components/WordsTable.jsx
+++ Wordhunt-Solver/src/components/WordsTable.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import "../styles/WordsTable.css";
 
+
 function WordsTable(props) {
 	let foundList = [];
 	for (let i = 0; i < props.foundWords.length; i++) {
@@ -15,13 +16,32 @@ function WordsTable(props) {
 		return b.word.length - a.word.length;
 	});
 
-	return <ul className="wordsTable">{wordList}</ul>;
-	let table = [];
-	foundList.map((i) => {
-		table.push(<li key={i.word}>{i.word}</li>);
-	});
+function groupWords(wordList){
+	let sortedList = [];
+	let tempList =[];
 
-	return <ul>{table}</ul>;
+	wordList.forEach(element => {
+		if(tempList.length === 0) tempList.push(element);
+		else if(tempList[tempList.length-1].length === element.length) tempList.push(element);
+		else{
+			sortedList.push(tempList);
+			tempList=[];
+			tempList.push(element);
+		}
+	});
+	if(tempList.length > 0) sortedList.push(tempList);
+	return sortedList;
+}
+
+const tableList = [...groupWords(props.foundWords)];
+
+	return (
+		<div className="wordsTable">
+			{tableList}
+			
+		</div>
+		
+		);
 }
 
 export default WordsTable;
